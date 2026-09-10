a4af3c035155a6ce451f6319b0852cb6a93f25bf
Siul0205 <118381264+Siul0205@users.noreply.github.com>
2026-09-10 14:20:20 -0500

    Create variables_en_python.py

---
+++ variables_en_python.py
@@ -0,0 +1,15 @@
+#Aqui aprenderemos lo que es una variable en python, una variable nos sirve para almacenar datos que necesitemos usar mas adelante
+#recomendable que una variable se utilice con guiones bajos, por ejemplo: nombre_variable o MyVariable
+
+#ejemplo de una variable
+nombre = "Luis" #Aqui le decimos al sistemas que nombre ahora significará "Luis"
+print(nombre) #Aqui confirmamos lo que significa nombre, en este caso nos imprimirá "Luis"
+
+#A mitad de nuestro programa podemos cambiar el valor de la variable, por ejemplo:
+nombre = "Siul" #Ahora le decimos al sistema que nombre ahora significará "Siul"
+print(nombre) #Aqui confirmamos lo que significa nombre, en este caso nos imprimirá "Siul"
+
+#podemos imprimir varias variables en una sola linea, por ejemplo:
+nombre = "Luis"
+apellido = "Siul"
+print(nombre, apellido) #esto nos imprimirá "Luis Siul" en la consola, imprimiendo el valor de las variables nombre y apellido
